613730689af4c07d7fff8f1f11ddfef2e2a6c691
DeepSaha25 <ideepsaha25@gmail.com>
2025-11-19 22:54:53 +0530

    initial3

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 setInterval(() => {
-    d = new Date();
-    htime = d.getHours();
-    mtime = d.getMinutes();
-    stime = d.getSeconds();
-    hrotation = 30*htime + mtime/2;
-    mrotation =6*mtime;
-    srotation =6*stime;
+    let d = new Date();
+    let htime = d.getHours();
+    let mtime = d.getMinutes();
+    let stime = d.getSeconds();
 
-    hour.style.transform =`rotate(${hrotation}deg)`;
-    minute.style.transform =`rotate(${mrotation}deg)`;
-    second.style.transform =`rotate(${srotation}deg)`;
+    let hrotation = 30 * htime + mtime / 2;
+    let mrotation = 6 * mtime;
+    let srotation = 6 * stime;
+
+    hour.style.transform = `translate(-50%, 0) rotate(${hrotation}deg)`;
+    minute.style.transform = `translate(-50%, 0) rotate(${mrotation}deg)`;
+    second.style.transform = `translate(-50%, 0) rotate(${srotation}deg)`;
 },1000);
